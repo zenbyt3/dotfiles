77ab03200c01d685b028a31562cf09160dd9d9c3
zenbyte <zenbytegithub.popcorn974@passmail.net>
2025-07-27 15:06:10 +0200

    add install script

---
+++ install.sh
@@ -0,0 +1,9 @@
+# Install Oh My Zsh manually
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Install Powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+
+# Install plugins manually  
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
